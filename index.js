8cf77a4f94ac638de71d170c7a0136c9d1d073ab
Haythem Bekir <65097270+haythembekir20@users.noreply.github.com>
2021-11-01 19:12:44 +0100

    v2

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
-mongoose.connect(
-    process.env.MONGO_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-      console.log("Connected to MongoDB");
-    }
-  );
 
+
+  mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+      console.log('Connected to Database')
+      app.listen(8800, () => {
+          console.log("You're listenzzing to port 8800")
+          
+      })
+});
   //middlawrre
 
   app.use(express.json());
@@ -25,17 +29,7 @@ mongoose.connect(
 
     app.use("/api/users", userRoute);
     app.use("/api/auth", authRoute);
-/*
-  app.get("/", (req,res) => {
-res.send("welcome to homepage");
-  });
+    app.use("/api/posts", postRoute);
 
-  app.get("/users", (req,res) => {
-    res.send("welcome to user page");
-      });
-      */
 
-app.listen(8800,()=>{
 
-    console.log("BACKEND IS RUNING")
-})
